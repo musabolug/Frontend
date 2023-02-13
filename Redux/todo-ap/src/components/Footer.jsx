78905636b77dbbbe5e005e217bfbc496448516c2
musabolug <musabolug@gmail.com>
2023-02-13 10:56:54 +0300

    main structure of the project is over. I am starting writing redux

---
+++ Redux/todo-ap/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer className='info'>
+        <p>Click to edit a todo</p>
+        <p>
+            created by <a href="www.github.com/musabolug">Musab OLuğ</a>
+        </p>
+
+    </footer>
+  )
+}
+
+export default Footer
---
+++ Redux/todo-ap/src/components/Form.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Form() {
+  return (
+    <form >
+    <input className='new-todo' placeholder='what needs to be done?' autofocus />
+</form>
+  )
+}
+
+export default Form
