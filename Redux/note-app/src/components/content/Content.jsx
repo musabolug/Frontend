f22e9e5e86f0c742f369f2687b770faea7537c92
musabolug <musabolug@gmail.com>
2023-02-16 14:27:31 +0300

    footer added

--- Redux/note-app/src/components/content/Content.jsx
+++ Redux/note-app/src/components/content/Content.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Header from './Header'
 import Form from './Form'
 import NoteList from "./NoteList";
+import Footer from './Footer';
 function Content() {
   return (
     <div className='content'>
       <Header />
       <Form/>
       <NoteList />
+      <Footer />
     </div>
   )
 }
---
+++ Redux/note-app/src/components/content/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <footer>
+         <p style={{color: "white"}}>
+            created by <a href="www.github.com/musabolug" style={{color: "white"}}>Musab OLuğ</a>
+        </p>
+        <br />
+    </footer>
+  )
+}
+
+export default Footer
