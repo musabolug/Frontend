f9afe023c28ba209f12c51a6bfa8bf97d88cd31c
musabolug <musabolug@gmail.com>
2023-02-16 18:22:16 +0300

    footer fixed

--- Redux/note-app/src/components/content/Footer.jsx
+++ Redux/note-app/src/components/content/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer>
          <p style={{color: "white"}}>
-            created by <a href="www.github.com/musabolug" style={{color: "white"}}>Musab OLuğ</a>
+            created by <a href="https://github.com/musabolug" style={{color: "white"}}>Musab OLuğ</a>
         </p>
         <br />
     </footer>
