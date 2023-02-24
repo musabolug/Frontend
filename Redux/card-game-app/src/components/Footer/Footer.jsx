137244c83ad80907c2f0258cf02564027f725311
musabolug <musabolug@gmail.com>
2023-02-24 16:17:53 +0300

    footer fixed

--- Redux/card-game-app/src/components/Footer/Footer.jsx
+++ Redux/card-game-app/src/components/Footer/Footer.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import "./footer.css"
 function Footer() {
   return (
     <footer>
     <p >
-       developed by <a href="https://github.com/musabolug" style={{color: "white"}}>Musab Olug</a>
+       developed by <a href="https://github.com/musabolug" style={{color: "white"}}><button className='link'>
+       Musab Olug
+</button></a>
    </p>
    <br />
 </footer>  )
