e12456067eed27ce31aa076ee1a337124ae771ab
musabolug <musabolug@gmail.com>
2023-03-09 09:35:27 +0300

    initial commit

---
+++ Redux/turkish-checkers-app/src/redux/gameSlice.jsx
@@ -0,0 +1,14 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const gameSlice = createSlice({
+    name:"game",
+    initialState:{
+
+    },
+    reducers:{
+
+    },
+})
+
+export const {} = gameSlice.actions
+export default gameSlice.reducer
---
+++ Redux/turkish-checkers-app/src/redux/store.jsx
@@ -0,0 +1,7 @@
+import { configureStore } from "@reduxjs/toolkit";
+import gameSlice from "./gameSlice";
+export const store = configureStore({
+    reducer:{
+        game: gameSlice
+    }
+})
