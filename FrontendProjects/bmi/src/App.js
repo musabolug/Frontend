b733481093cd8fe7f8cc97346b3ae1530167c15b
musabolug <musabolug@gmail.com>
2023-05-23 14:48:52 +0300

    icon

--- FrontendProjects/bmi/src/App.js
+++ FrontendProjects/bmi/src/App.js
@@ -5,18 +5,23 @@ import About from './Pages/About';
 import DietList from './Pages/DietList';
 function App() {
   return (
-    <>
+    <div>
+   
     <nav>
-      <NavLink to="/"><img src="" alt="" /></NavLink>
+      <NavLink to="/"><img className='logo' src="https://github.com/musabolug/Frontend/blob/master/FrontendProjects/bmi/public/logo.png?raw=true" alt="" /></NavLink>
+      <div>
+      
+      <NavLink to="/">BMI hesapla</NavLink>
       <NavLink to="/about">BMI Nedir?</NavLink>
       <NavLink to="/diet-list">Diyet Listesi</NavLink>
+      </div>
     </nav>
     <Routes>
       <Route path='/' element={<Main/>}/>
       <Route path='/about' element={<About/>}/>
       <Route path='/diet-list' element={<DietList/>}/>
     </Routes>
-    </>
+    </div>
   )
 }
 
