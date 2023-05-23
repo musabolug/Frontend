4500d63cc81c3cffd592f140e399513e3e166f81
musabolug <musabolug@gmail.com>
2023-05-23 14:33:30 +0300

    initial commit

---
+++ FrontendProjects/bmi/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import { Route, Routes , NavLink } from 'react-router-dom';
+import Main from './Pages/Main';
+import About from './Pages/About';
+import DietList from './Pages/DietList';
+function App() {
+  return (
+    <>
+    <nav>
+      <NavLink to="/"></NavLink>
+      <NavLink to="/about">BMI Nedir?</NavLink>
+      <NavLink to="/diet-list">Diyet Listesi</NavLink>
+    </nav>
+    <Routes>
+      <Route path='/' element={<Main/>}/>
+      <Route path='/about' element={<About/>}/>
+      <Route path='/diet-list' element={<DietList/>}/>
+    </Routes>
+    </>
+  )
+}
+
+export default App;
