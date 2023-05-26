58668b7cf8dc263c358e10bb9128d5014d441333
musabolug <musabolug@gmail.com>
2023-05-26 16:45:52 +0300

    portfolio site

---
+++ portfolio/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import {NavBar} from "./components/navigationBar"
+import "bootstrap/dist/css/bootstrap.min.css"
+import Banner from "./components/Banner"
+function App() {
+  return (
+    <div className="App">
+    <NavBar/>
+    {/* <Banner /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ portfolio/src/components/Banner.jsx
@@ -0,0 +1,58 @@
+/* eslint-disable no-unused-vars */
+import {useState, useEffect} from 'react'
+import {Container , Row, Col} from "react-bootstrap"
+import {ArrowRightCircle} from "react-bootstrap-icons"
+import HeaderImg from "../assets/img/header-img.svg"
+
+function Banner() {
+    const [loopNum , setLoopNum] = useState(0)
+    const [isDeleting ,setIsDeleting] = useState(false)
+  const  toRotate = ["Web Developer","Web Designer" ,"UI/UX Designer"]    
+  const[text,setText] = useState("")
+  const period = 2000;
+  const [delta, setDelta] = useState(300 - Math.random() * 100)
+  useEffect(()=>{
+    let ticker = setInterval(()=>{
+        tick()
+    },delta)
+    return () => {clearInterval(ticker)}
+  },[text])
+
+  const  tick = () =>{
+    let i = loopNum % toRotate.length;
+    let fullText =  toRotate(i);
+    let updatedText = isDeleting? fullText.substring(0,text.length - 1 ) : fullText.substring(0, text.length + 1)
+
+    setText(updatedText) 
+
+    if(isDeleting){
+        setDelta(prevDelta => prevDelta / 2 ) 
+    }
+
+    if(!isDeleting && updatedText === fullText){
+        setIsDeleting(true)
+        
+    }
+  } 
+
+  return (
+    <section className='banner' id='home'> 
+    <Container>
+     <Row className='align-items-center'>
+        <Col xs={12} md={6} xl={7}>
+            <span className='tagline'>Welcome to my Portfolio</span>
+            <h1>{"Hi I'm Musabolug"} <span className='wrap'>web developer</span> </h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nesciunt rerum itaque ipsam eligendi, accusamus ut.</p>
+            <button onClick={()=> console.log("Connect")}>Let’s Connect <ArrowRightCircle size={25} /></button>
+        </Col>
+        <Col xs={12} md={6} xl={5}>
+            <img src={HeaderImg} alt="Header Img" />
+        </Col>
+     </Row>
+    </Container>
+
+    </section>
+  )
+}
+
+export default Banner
